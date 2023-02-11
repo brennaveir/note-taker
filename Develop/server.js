12d3fbedb2cfa0f35132af819d91b4b3ee5ca395
brennaveir <brennaveir@hotmail.com>
2023-02-11 16:15:10 -0500

    changed route to log get request

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const routes = require('./routes');
 
 
 const PORT = 3001;
@@ -10,20 +10,20 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
-
+//GET * should return the index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 //TODO: HTML routes
 //TODO: GET /notes should return the notes.html file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
-//GET * should return the index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+
+app.use(routes)
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
